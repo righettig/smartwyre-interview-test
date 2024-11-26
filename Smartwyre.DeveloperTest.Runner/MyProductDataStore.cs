06e8263a900086e4a445173ddfea60a0cba966e4
Giacomo Righetti <righetti.giacomo@live.com>
2024-11-26 18:34:01 +0100

    Added fake data

--- Smartwyre.DeveloperTest.Runner/MyProductDataStore.cs
+++ Smartwyre.DeveloperTest.Runner/MyProductDataStore.cs
@@ -1,16 +1,55 @@
 ﻿using Smartwyre.DeveloperTest.Data.Interfaces;
 using Smartwyre.DeveloperTest.Types;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Smartwyre.DeveloperTest.Runner;
 
 public class MyProductDataStore : IProductDataStore
 {
+    private readonly List<Product> products;
+
+    public MyProductDataStore()
+    {
+        products =
+        [
+            new Product
+            {
+                Id = 1,
+                Identifier = "product1",
+                Price = 100.00m,
+                Uom = "kg",
+                SupportedIncentives = SupportedIncentiveType.FixedRateRebate
+            },
+            new Product
+            {
+                Id = 2,
+                Identifier = "product2",
+                Price = 50.00m,
+                Uom = "item",
+                SupportedIncentives = SupportedIncentiveType.FixedCashAmount
+            },
+            new Product
+            {
+                Id = 3,
+                Identifier = "product3",
+                Price = 200.00m,
+                Uom = "liter",
+                SupportedIncentives = SupportedIncentiveType.ShinyNewIncentiveType
+            },
+            new Product
+            {
+                Id = 4,
+                Identifier = "product4",
+                Price = 75.00m,
+                Uom = "box",
+                SupportedIncentives = SupportedIncentiveType.AmountPerUom
+            }
+        ];
+    }
+
     public Product GetProduct(string productIdentifier)
     {
-        // Access database to retrieve account, code removed for brevity 
-        return new Product() 
-        { 
-            SupportedIncentives = SupportedIncentiveType.ShinyNewIncentiveType 
-        };
+        return products.FirstOrDefault(x => x.Identifier == productIdentifier);
     }
 }
--- Smartwyre.DeveloperTest.Runner/MyRebateDataStore.cs
+++ Smartwyre.DeveloperTest.Runner/MyRebateDataStore.cs
@@ -1,21 +1,53 @@
 ﻿using Smartwyre.DeveloperTest.Data.Interfaces;
 using Smartwyre.DeveloperTest.Types;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Smartwyre.DeveloperTest.Runner;
 
 public class MyRebateDataStore : IRebateDataStore
 {
+    private readonly List<Rebate> rebates;
+
+    public MyRebateDataStore()
+    {
+        rebates =
+        [
+            new Rebate
+            {
+                Identifier = "rebate1",
+                Incentive = IncentiveType.FixedRateRebate,
+                Amount = 0, // Amount is not applicable for FixedRateRebate
+                Percentage = 10.0m
+            },
+            new Rebate
+            {
+                Identifier = "rebate2",
+                Incentive = IncentiveType.AmountPerUom,
+                Amount = 5.0m,
+                Percentage = 0 // Percentage is not applicable for AmountPerUom
+            },
+            new Rebate
+            {
+                Identifier = "rebate3",
+                Incentive = IncentiveType.FixedCashAmount,
+                Amount = 50.0m,
+                Percentage = 0 // Percentage is not applicable for FixedCashAmount
+            },
+            new Rebate
+            {
+                Identifier = "rebate4",
+                Incentive = IncentiveType.ShinyNewIncentiveType,
+                Amount = 25.0m,
+                Percentage = 5.0m
+            }
+        ];
+    }
+
     public Rebate GetRebate(string rebateIdentifier)
     {
-        // Access database to retrieve account, code removed for brevity 
-        var rebate = new Rebate
-        {
-            Identifier = rebateIdentifier,
-            Amount = 1000,
-            Percentage = 1,
-            Incentive = IncentiveType.ShinyNewIncentiveType,
-        };
+        var rebate = rebates.FirstOrDefault(x => x.Identifier == rebateIdentifier);
 
         Console.WriteLine(
             $"Retriving rebate: Identifier {rebate.Identifier}, Amount: {rebate.Amount}, Percentage: {rebate.Percentage}, Incentive: {rebate.Incentive}");
@@ -26,7 +58,5 @@ public class MyRebateDataStore : IRebateDataStore
     public void StoreCalculationResult(Rebate account, decimal rebateAmount)
     {
         Console.WriteLine($"Rebate amount: {rebateAmount}");
-
-        // Update account in database, code removed for brevity
     }
 }
